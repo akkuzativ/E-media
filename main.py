44d7ce083888045d3d7c8d3ab5780e749f2bdce3
Aleksy <goreckialeksander9@gmail.com>
2021-03-20 23:08:40 +0100

    Demo

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-import os
-import io
+from matplotlib import pyplot as plt
+import numpy as np
 
 
 f = open(file="sine440.wav", mode="rb")
-print(f.readline()[0:4])
+f.seek(16)
+sample_len = int(int.from_bytes(f.read(4), byteorder="little", signed=False)/8)
+f.seek(44)
+samples = []
+data = f.read(-1)
+
+pos = 0
+while pos < len(data):
+    samples.append(int.from_bytes(data[pos:pos+sample_len], byteorder="little", signed=True))
+    pos = pos + sample_len
+
+plt.plot(list(range(0, len(samples), 1))[0:110], samples[0:110])
+plt.show()
