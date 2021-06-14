58da558a310c953ffe26cd5b324b6f72e948b3ff
Aleksy <goreckialeksander9@gmail.com>
2021-06-14 23:08:37 +0200

    Ignorowanie szumu w widmie fazowym, dodatkowe pliki testowe

--- main.py
+++ main.py
@@ -1030,7 +1030,7 @@ class id3Chunk(Chunk):
 
 size = 0
 sample_len = 0
-f = open(file="data/sine440.wav", mode="rb")
+f = open(file="data/sine440-shifted.wav", mode="rb")
 
 while 1:
     id = bytes.decode(f.read(4))
@@ -1222,10 +1222,16 @@ def display_waveform(dataChunk: DataChunk, fmtChunk: FmtChunk, lower: int = None
     plt.show(block=True)
 
 
-def display_spectrogram(dataChunk: DataChunk, fmtChunk: FmtChunk):
+def display_spectrogram(dataChunk: DataChunk, fmtChunk: FmtChunk, lower, upper):
     plt.close()
     channels = np.array(dataChunk.data.samples)
 
+    if lower is None or upper is None:
+        lower = 0
+        upper = len(channels[0])
+    elif lower < 0 or upper < 0 or lower > len(channels[0]) or upper > len(channels[0]):
+        lower = 0
+        upper = len(channels[0])
 
     time_axis = (np.array(range(0, len(channels[0])))/fmtChunk.data.sample_rate)[lower:upper]
     figure, axes = plt.subplots(len(channels), 1, sharex=False, sharey=True)
@@ -1233,6 +1239,7 @@ def display_spectrogram(dataChunk: DataChunk, fmtChunk: FmtChunk):
 
     if fmtChunk.data.num_channels == 1:
         channel = channels[0]
+        channel = channel[lower:upper]
         channel = normalize_samples(channel, fmtChunk)
         axes.specgram(x=channel, Fs=fmtChunk.data.sample_rate, scale="dB")
         axes.set_yscale("symlog")
@@ -1240,13 +1247,14 @@ def display_spectrogram(dataChunk: DataChunk, fmtChunk: FmtChunk):
         axes.set_xlabel("Czas [s]")
     else:
         for channel_index, channel in enumerate(channels):
+            channel = channel[lower:upper]
             channel = normalize_samples(channel, fmtChunk)
             axes[channel_index].specgram(x=channel, Fs=fmtChunk.data.sample_rate, scale="dB")
             axes[channel_index].set_yscale("symlog")
             axes[channel_index].set_title(f"Kanał {channel_index+1}")
             axes[channel_index].set_ylabel("Częstotliwość [Hz]")
             axes[channel_index].set_xlabel("Czas [s]")
-    plt.suptitle("Spektrogram całości sygnału wewnątrz pliku")
+    plt.suptitle("Spektrogram wybranego fragmentu sygnału wewnątrz pliku")
     plt.tight_layout()
     plt.show(block=True)
 
@@ -1278,9 +1286,9 @@ def display_amplitude_spectrum(dataChunk: DataChunk, fmtChunk: FmtChunk, lower: 
         for channel_index, channel in enumerate(channels):
             channel = channel[lower:upper]
             #channel = normalize_samples(channel, fmtChunk)
-            amplitude = scipy.fft.rfft(channel)
+            spectrum = scipy.fft.rfft(channel)
             frequencies = scipy.fft.rfftfreq(len(channel), 1/fmtChunk.data.sample_rate)
-            axes[channel_index].plot(frequencies, np.abs(amplitude))
+            axes[channel_index].plot(frequencies, np.abs(spectrum))
             axes[channel_index].set_xscale("symlog")
             axes[channel_index].set_title(f"Kanał {channel_index+1}")
             axes[channel_index].set_ylabel("Amplituda")
@@ -1307,9 +1315,13 @@ def display_phase_spectrum(dataChunk: DataChunk, fmtChunk: FmtChunk, lower: int 
     if fmtChunk.data.num_channels == 1:
         channel = channels[0][lower:upper]
         normalize_samples(channel, fmtChunk)
-        amplitude = scipy.fft.rfft(channel)
+        spectrum = scipy.fft.rfft(channel)
+        threshold = max(abs(spectrum)) / 100
+        for i, x in enumerate(spectrum):
+            if x < threshold:
+                spectrum[i] = 0
         frequencies = scipy.fft.rfftfreq(len(channel), 1 / fmtChunk.data.sample_rate)
-        axes.plot(frequencies, np.angle(amplitude))
+        axes.plot(frequencies, np.angle(spectrum))
         axes.set_xscale("symlog")
         axes.set_ylabel("Przesunięcie fazowe [rad]")
         axes.set_xlabel("Częstotliwość [Hz]")
@@ -1318,6 +1330,10 @@ def display_phase_spectrum(dataChunk: DataChunk, fmtChunk: FmtChunk, lower: int 
             channel = channel[lower:upper]
             channel = normalize_samples(channel, fmtChunk)
             spectrum = scipy.fft.rfft(channel)
+            threshold = max(abs(spectrum))/100
+            for i, x in enumerate(spectrum):
+                if x < threshold:
+                    spectrum[i] = 0
             frequencies = scipy.fft.rfftfreq(len(channel), 1/fmtChunk.data.sample_rate)
             axes[channel_index].plot(frequencies, np.angle(spectrum))
             axes[channel_index].set_xscale("symlog")
@@ -1349,7 +1365,7 @@ except:
 display_waveform(dataChunk, fmtChunk, lower, upper)
 display_amplitude_spectrum(dataChunk, fmtChunk, lower, upper)
 display_phase_spectrum(dataChunk, fmtChunk, lower, upper)
-display_spectrogram(dataChunk, fmtChunk)
+display_spectrogram(dataChunk, fmtChunk, lower, upper)
 
 ###
 # zapis
