3c8b8e0eea6e2d0c8d9316817db80a3acca5d6c0
Konrad Bialek <konrad.5606@gmail.com>
2021-06-20 20:22:12 +0200

    częściowa zmiana rsa

--- RSA.py
+++ RSA.py
@@ -1,36 +1,41 @@
 import random
+import secrets
 import sys
 import math
 from random import randrange
 
 
-def rabinMiller(n, k=10):
-    if n == 2:
+def test(a, r, d, num):
+    x = pow(a, d, num)
+    for i in range(1, r - 1):
+        if x == num - 1:
             return True
-    if not n & 1:
-            return False
+        x = pow(x, 2, num)
+    return x == 1 or x == num - 1
 
-    def check(a, s, d, n):
-            x = pow(a, d, n)
-            if x == 1:
-                    return True
-            for i in range(1, s - 1):
-                    if x == n - 1:
-                            return True
-                    x = pow(x, 2, n)
-            return x == n - 1
 
-    s = 0
-    d = n - 1
+def rabinMiller(num, iterations=100):
+    """
+    :param num: liczba
+    :param iterations: liczba iteracji
+    :return: True - prawdopodobnie pierwsza; False - złożona
+    """
+    if num == 2:
+        return True
+    if not num & 1:
+        return False
+
+    r = 0  # potęga 2 w num = 2**r*d+1
+    d = num - 1  # ----------------^
 
     while d % 2 == 0:
-            d >>= 1
-            s += 1
+        d >>= 1  # dziel przez 2**1
+        r += 1
 
-    for i in range(1, k):
-            a = randrange(2, n - 1)
-            if not check(a, s, d, n):
-                    return False
+    for i in range(1, iterations):
+        a = randrange(2, num - 1)
+        if not test(a, r, d, num):
+            return False
     return True
 
 
@@ -58,9 +63,11 @@ def inverse(a, b):
         ly += oa  # If neg wrap modulo orignal a
     # return a , lx, ly  # Return only positive values
     return lx
-# def is_prime(n):
+
+
+# def is_prime(num):
 #     # lowPrimes is all primes (sans 2, which is covered by the bitwise and operator)
-#     # under 1000. taking n modulo each lowPrime allows us to remove a huge chunk
+#     # under 1000. taking num modulo each lowPrime allows us to remove a huge chunk
 #     # of composite numbers from our potential pool without resorting to Rabin-Miller
 #     lowPrimes = [3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
 #         , 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179
@@ -72,61 +79,58 @@ def inverse(a, b):
 #         , 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773
 #         , 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883
 #         , 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997]
-#     if (n >= 3):
-#         if (n & 1 != 0):
+#     if (num >= 3):
+#         if (num & 1 != 0):
 #             for p in lowPrimes:
-#                 if (n == p):
+#                 if (num == p):
 #                     return True
-#                 if (n % p == 0):
+#                 if (num % p == 0):
 #                     return False
-#             return rabinMiller(n)
+#             return rabinMiller(num)
 #     return False
 
 
 def choose(size):
-    r = 100 * (math.log(size, 2) + 1)  # liczba prób
-    while r > 0:
-        # randrange tylko chwilowo
-        n = random.randrange(2 ** (size - 1), 2 ** (size))
-        r -= 1
-        # if is_prime(n) == True:
-        if rabinMiller(n) == True:
-                return n
+    for _ in reversed(range(int(100 * (math.log(size, 2) + 1)))):  # liczba prób
+        n = secrets.randbelow(2 ** size)
+        while n <= 2 ** (size - 1):
+            n = secrets.randbelow(2 ** size)
+        # if is_prime(num) == True:
+        if rabinMiller(n):
+            return n
 
 
 def choose_prime_numbers(size):
     # wybór liczb pierwszych
     p = choose(size)
-    print(p)
     q = choose(size)
-    print(q)
-    if p == q:
+    while p == q:
         print('p and q are equal. Recalculate q.')
         q = choose(size)
-        print(q)
 
-    # wyznaczenie n
-    # n = multiply(p, q)
-    n = p*q
+    # wyznaczenie num
+    # num = multiply(p, q)
+    n = p * q
 
     # zakres losowania
-    # range = multiply((p-1),(q-1))
-    range = (p-1)*(q-1)
+    # drawrange = multiply((p-1),(q-1))
+    drawrange = (p - 1) * (q - 1)
     g = 0
     while g != 1:
         # względnie pierwsza e
-        e = random.randrange(1, range)
-        # czy względnie pierwsza (Euclid's Algorithm)
-        g = gcd(e, range)
+        e = random.randrange(1, drawrange)
+        # czy względnie pierwsza (Euclid'r Algorithm)
+        g = gcd(e, drawrange)
 
     # część prywatna
-    d = inverse(e, range)
+    d = inverse(e, drawrange)
 
-    #Zwróć klucze odpowiednio publiczny i prywatny
-    return (e, n), (d, n)
+    # Zwróć klucze odpowiednio publiczny i prywatny i paręliczb
+    return (e, n), (d, n), (p, q)
 
 
 def fun():
-    public, private = choose_prime_numbers(100)
+    public, private, prime = choose_prime_numbers(100) # argument to potęga 2 odpowiadająca górnej granicy liczb
+
 
 choose_prime_numbers(100)
